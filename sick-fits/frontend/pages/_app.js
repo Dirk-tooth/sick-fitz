b5ca8295dca7c031ec1001e7beb62aae1f63c57c
Trevor Davis <trevormckay.davis@gmail.com>
2020-06-16 09:35:12 -0500

    React + Apollo + Yoga + Prisma setup

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,42 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
-class MyApp extends App {
-  render() {
-    const { Component } = this.props;
-
-    return (
-      <Container>
+function MyApp({ apollo, Component, pageProps }) {
+  return (
+    <Container>
+      <ApolloProvider client={apollo}>
         <Page>
-          <Component />
+          <Component {...pageProps} />
         </Page>
-      </Container>
-    );
-  }
+      </ApolloProvider>
+    </Container>
+  );
 }
 
-export default MyApp;
+// class MyApp extends App {
+//   static async getInitialProps({ Component, ctx }) {
+//     if (Component.getInitialProps) {
+//       pageProps = await Component.getInitialProps;
+//     }
+//     // this exposes the query to the user
+//     pageProps.query = ctx.query;
+//     return { pageProps };
+//   }
+//   render() {
+//     const { Component, apollo, pageProps } = props;
+
+//     return (
+//       <Container>
+//         <ApolloProvider client={apollo}>
+//           <Page>
+//             <Component {...pageProps} />
+//           </Page>
+//         </ApolloProvider>
+//       </Container>
+//     );
+//   }
+// }
+
+export default withData(MyApp);
